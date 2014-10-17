ff328b16eb70c3a27b78f1b95d5f1ddee1bed8a2
AllenLin001 <allenlin41@yahoo.com>
2014-10-17 14:07:29 -0500

    doc finished

--- CityscapeLab/Moon.java
+++ CityscapeLab/Moon.java
@@ -3,27 +3,38 @@ import java.awt.geom.Ellipse2D;
 import java.awt.Color;
 
 /**
- * Constructs a Moonthat can be displaced anywhere on the screen at ()
+ * a circle shape Moon that can be displaced anywhere on the screen at ()
  * Allen Lin  
  * 10/18/14 
  */
 
 public class Moon
 {
-    //  instancevar xLeft the x-coordinate of the center of the moon on the screen. 
-    //  instancevar yTop the y-coordinate of the center of the moon on the screen. 
-
-    private int xLeft; 
-    private int yTop; 
+    /**
+      instancevar xLeft the x-coordinate of the center of the moon on the screen. 
+      instancevar yTop the y-coordinate of the center of the moon on the screen. 
+    */
     
-   // param@ x the x coordinate of center of the moon. 
-   // param@ y the y coordinate of the moon.   
+    private int xLeft; 
+    private int yTop;
+    
+    /**
+       Constrcuts a moon with a given center.
+       param@ x the x coordinate of center of the moon. 
+       param@ y the y coordinate of the moon.   
+    */
+    
     public Moon (int x, int y)
     {
         this.xLeft = x;
         this.yTop = y;
     }
     
+    /**
+       Draws the moon.
+       @param g2 the graphics context. 
+    */
+
     public void draw(Graphics2D g2)
     {
         Ellipse2D.Double moon = new Ellipse2D.Double(xLeft+20,yTop, 120,120);
