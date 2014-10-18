4e4b259ffa1d5ead65f7e3abe923846fd1359a9f
AllenLin001 <allenlin41@yahoo.com>
2014-10-17 21:25:59 -0500

    java.doc completed

--- CityscapeLab/Building.java
+++ CityscapeLab/Building.java
@@ -12,36 +12,40 @@ import java.awt.Graphics2D;
  
 public class Building
 {
-    /** int specify the starting pt of x */
+    /** instancevar xLeft specify the x-coordinate of the top left corner. */
     private int xLeft;
     
-    /** int specify the top value */ 
+    /** instancevar yTop specify the y-coordinate of the top left corner. */ 
     private int yMin;
     
-    /** int specify the height of the frame*/
-    private int frameHeight;
+    /** int specify the height of the building*/
+    private int bHeight;
     
     /** int specify the width of the building*/
     private int bWidth;
    
     /**
-     * Constructor for the class
-     * @param frameHeight the height of the frame
+     * Constructor the rectangluar building with a given (x,y) and height and width. 
+     * @param bHeight the height of the building
      * @param bWidth the width of the building
-     * @param xLeft staring x coordinate
-     * @param yTop staring pt for y
+     * @param xLeft x coordinate of the top left corner
+     * @param yTop y coordinate of the top left corner
      */
     public Building (int xLeft, int yMin, int width, int height)
     {
-        this.frameHeight= height;
+        this.bHeight= height;
         this.bWidth= width;
         this.xLeft=xLeft;
         this.yMin=yMin;
     }
     
+    /**
+     * draws the building and sets its color to blue.
+     * @param g2 the graphics context
+    */
     public void draw (Graphics2D g2)
     {
-        Rectangle building = new Rectangle(this.xLeft,this.yMin,this.bWidth,this.frameHeight);
+        Rectangle building = new Rectangle(this.xLeft,this.yMin,this.bWidth,this.bHeight);
         g2.setColor(Color.BLUE);
         g2.draw(building);
         g2.fill(building);
